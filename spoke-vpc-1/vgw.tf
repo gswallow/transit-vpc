b6762e45b811ff0ac978911918da2b89d070819f
Greg Swallow <gswallow@ivytech.edu>
2018-05-03 11:01:37 -0400

    Enable BGP route propagation

--- spoke-vpc-1/vgw.tf
+++ spoke-vpc-1/vgw.tf
@@ -1,7 +1,14 @@
 resource "aws_vpn_gateway" "vpn_gw" {
   vpc_id = "${aws_vpc.spoke_1.id}"
+  amazon_side_asn = 65110
   tags {
     "Name" = "${format("%s-%s-to-transit-vgw", var.ORG, data.aws_region.current.name)}"
     "Organization" = "${var.ORG}"
   }
 }
+
+resource "aws_vpn_gateway_route_propagation" "public" {
+  vpn_gateway_id = "${aws_vpn_gateway.vpn_gw.id}"
+  route_table_id = "${aws_route_table.public_rtb.id}"
+}
+
--- spoke-vpc-2/vgw.tf
+++ spoke-vpc-2/vgw.tf
@@ -1,7 +1,13 @@
 resource "aws_vpn_gateway" "vpn_gw" {
   vpc_id = "${aws_vpc.spoke_2.id}"
+  amazon_side_asn = 65120
   tags {
     "Name" = "${format("%s-%s-to-transit-vgw", var.ORG, data.aws_region.current.name)}"
     "Organization" = "${var.ORG}"
   }
+}
+
+resource "aws_vpn_gateway_route_propagation" "public" {
+  vpn_gateway_id = "${aws_vpn_gateway.vpn_gw.id}"
+  route_table_id = "${aws_route_table.public_rtb.id}"
 }
