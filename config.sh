ccef7a63113df5fe9b71cf593b122c07fad184f1
Greg Swallow <gswallow@ivytech.edu>
2018-05-03 16:24:50 -0400

    Parameterize interface names

--- config.sh
+++ config.sh
@@ -7,12 +7,14 @@ VPN_ID=vpn-ebe135dc
 LOCAL_BGP_AS=65100
 REMOTE_BGP_AS=65110
 
+TUN1_INTERFACE=vti0
 TUN1_INTERFACE_LOCAL_ADDR=169.254.59.150
 TUN1_INTERFACE_REMOTE_ADDR=169.254.59.149
 TUN1_PEER_ADDR=18.216.212.249
 TUN1_INTERFACE_CIDR=30
 TUN1_SHARED_SECRET=lBx1h3CVgKXJ1Cm47Q4WQhrVUDj54oHN
 
+TUN2_INTERFACE=vti1
 TUN2_INTERFACE_LOCAL_ADDR=169.254.59.106
 TUN2_INTERFACE_REMOTE_ADDR=169.254.59.105
 TUN2_PEER_ADDR=52.14.142.186
@@ -32,10 +34,10 @@ set vpn ipsec nat-traversal enable
 
 # Tunnel 1
 
-set interfaces vti vti0 description ${VPN_ID}-1
-set interfaces vti vti0 address ${TUN1_INTERFACE_LOCAL_ADDR}/${TUN1_INTERFACE_CIDR}
-set interfaces vti vti0 mtu 1436
-set interfaces vti vti0 ip source-validation disable
+set interfaces vti $TUN1_INTERFACE description ${VPN_ID}-1
+set interfaces vti $TUN1_INTERFACE address ${TUN1_INTERFACE_LOCAL_ADDR}/${TUN1_INTERFACE_CIDR}
+set interfaces vti $TUN1_INTERFACE mtu 1436
+set interfaces vti $TUN1_INTERFACE ip source-validation disable
 
 set vpn ipsec ike-group ${VPN_ID}-1 proposal 1
 set vpn ipsec ike-group ${VPN_ID}-1 proposal 1 encryption aes128
@@ -60,7 +62,7 @@ set authentication id $ENI_ADDR
 set authentication remote-id $TUN1_PEER_ADDR
 set ike-group ${VPN_ID}-1
 set local-address $ENI_ADDR
-set vti bind vti0
+set vti bind $TUN1_INTERFACE
 set vti esp-group ${VPN_ID}-1
 top
 
@@ -70,10 +72,10 @@ set protocols bgp $LOCAL_BGP_AS neighbor $TUN1_INTERFACE_REMOTE_ADDR update-sour
 
 # Tunnel 2 
 
-set interfaces vti vti1 description ${VPN_ID}-2
-set interfaces vti vti1 address ${TUN2_INTERFACE_LOCAL_ADDR}/${TUN2_INTERFACE_CIDR}
-set interfaces vti vti1 mtu 1436
-set interfaces vti vti1 ip source-validation disable
+set interfaces vti $TUN2_INTERFACE description ${VPN_ID}-2
+set interfaces vti $TUN2_INTERFACE address ${TUN2_INTERFACE_LOCAL_ADDR}/${TUN2_INTERFACE_CIDR}
+set interfaces vti $TUN2_INTERFACE mtu 1436
+set interfaces vti $TUN2_INTERFACE ip source-validation disable
 
 set vpn ipsec ike-group ${VPN_ID}-2 proposal 1
 set vpn ipsec ike-group ${VPN_ID}-2 proposal 1 encryption aes128
@@ -95,7 +97,7 @@ edit vpn ipsec site-to-site peer $TUN2_PEER_ADDR
 set authentication pre-shared-secret $TUN2_SHARED_SECRET
 set ike-group ${VPN_ID}-2
 set local-address $ENI_ADDR
-set vti bind vti1
+set vti bind $TUN2_INTERFACE
 set vti esp-group ${VPN_ID}-2
 top
 
